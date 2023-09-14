2a272a4a659e0375d23b9e3d4aee80026fabf361
PavanS1609 <pavankumar.springboot@gmail.com>
2023-09-14 08:27:44 +0530

    Add project files.

---
+++ MultiCastDelegate.cs
@@ -0,0 +1,43 @@
+﻿
+using System;
+
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+ public delegate void DelegatesDemoDelegate(int x, int y);
+    internal class MultiCastDelegate
+    {
+        public static void ImplementDelegate()
+        {
+            DelegatesDemoDelegate obj = Operations.Add;
+            obj += Operations.Multiplication;
+            obj += Operations.Divide;
+
+            obj(10, 3);
+            //obj(2, 5);
+        }
+
+
+        public class Operations
+        {
+            public static void Add(int a, int b)
+            {
+                Console.WriteLine("Addition is :" + (a + b));
+            }
+
+            public static void Multiplication(int a, int b)
+            {
+                Console.WriteLine("Multiplication is :" + a * b);
+            }
+            public static void Divide(int a, int b)
+            {
+                Console.WriteLine("Division is : " + (a / b));
+            }
+        }
+    }
+
+
+
+
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+public delegate int Operation(int x, int y);
+internal class Program
+{
+    public static int Addition(int a, int b)
+    {
+        return a + b;
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Delegate Demo");
+
+        Operation delegateobj = new Operation(Addition);
+
+        int output = delegateobj.Invoke(2, 6);
+
+        Console.WriteLine("Addition is : " + output);
+    }
+}
